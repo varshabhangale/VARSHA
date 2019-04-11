9a38ccef0ee42afe0565665e066046e747801284
varshabhangale <varbhangale9@gmail.com>
2019-04-11 17:45:57 +0530

    p1.java

---
+++ p1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class p1
+{
+	public static void main(String [] args)
+		{
+			int x;
+
+			Scanner scan=new Scanner(System.in);
+
+			System.out.println("Enter x:");
+			
+			x=scan.nextInt();
+			
+		if(x>0)
+			{
+				
+				System.out.println("Number is positive");
+			}		
+else
+			{
+				System.out.println("Number is negative");
+			}
+		}
+}
